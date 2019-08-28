2bcd8d800c959dbd9c362d99e6e404bbb262d274
fwelpe <wheel0@mail.ru>
2019-08-29 01:26:28 +0300

    base done

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const Square = (props) => {
     return (
         <button
             className="square"
+            onClick={props.onClick}
         >
             {props.value}
         </button>
@@ -17,31 +18,92 @@ class Calc extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            squares: Array(9).fill(null),
+            expr: Array(1).fill(0),
         };
     }
 
+    handleClick(v) {
+        if (!v)
+            return;
+        let expr_array = [...this.state.expr];
+        if (Number.isInteger(v)) {
+            if (!Number.isInteger(expr_array[expr_array.length - 1]))
+                expr_array.push(0);
+            expr_array[expr_array.length - 1] = expr_array[expr_array.length - 1] * 10 + v;
+        } else if (v === 'C')
+            expr_array = [0];
+        else if (v !== '=')
+            expr_array.push(v);
+        else {
+            expr_array = [this.result()]
+        }
+        this.setState(
+            {
+                expr: expr_array
+            }
+        );
+    }
+
+    result() {
+        if (this.state.expr.length === 1)
+            return null;
+        let result = 0;
+        let op;
+        const expr_array = [...this.state.expr];
+        let i = -1;
+        while (++i < expr_array.length) {
+            if (!Number.isInteger(expr_array[i]))
+                op = expr_array[i];
+            else if (op) {
+                if (op === '+')
+                    result += expr_array[i];
+                else if (op === '-')
+                    result -= expr_array[i];
+            } else {
+                result += expr_array[i];
+            }
+        }
+        return result;
+    }
+
     renderSquare(i) {
-        return (<Square />)
+        return (
+            <Square
+                value={i}
+                onClick={() => this.handleClick(i)}
+            />
+        )
     }
 
     render() {
         return (
             <div>
+                <div className="status">{this.state.expr.join(' ')}</div>
+                <div className="result">{this.result()}</div>
                 <div className="board-row">
-                    {this.renderSquare(0)}
+                    {this.renderSquare('+')}
+                    {this.renderSquare('-')}
+                    {this.renderSquare('=')}
+                </div>
+                <div className="board-row">
                     {this.renderSquare(1)}
                     {this.renderSquare(2)}
+                    {this.renderSquare(3)}
                 </div>
                 <div className="board-row">
-                    {this.renderSquare(3)}
                     {this.renderSquare(4)}
                     {this.renderSquare(5)}
+                    {this.renderSquare(6)}
                 </div>
                 <div className="board-row">
-                    {this.renderSquare(6)}
                     {this.renderSquare(7)}
                     {this.renderSquare(8)}
+                    {this.renderSquare(9)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare('C')}
+                    {this.renderSquare(0)}
+                    {this.renderSquare()}
                 </div>
             </div>);
     }
